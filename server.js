fc51b0ccf69f816ff9005c3765fb9bf1d6a091cf
Shubham Mishra <meshuham@duck.com>
2024-11-22 18:41:38 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const errorHandler = require("./middleware/errorHandler");
+const dotenv = require("dotenv").config();
+
+const app = express();
+const cors = require('cors');
+
+// Allow specific origin
+app.use(cors({
+  origin: 'https://bfhl-gold.vercel.app', // Your frontend URL
+  methods: ['GET', 'POST'], // Allowed methods
+}));
+
+// Or allow all origins (not recommended in production)
+app.use(cors());
+
+
+
+const port = process.env.PORT || 2000;
+
+app.use(express.json());
+
+
+app.use("/",require("./router/router"));
+
+app.use(errorHandler);
+
+
+app.listen(port, () =>{
+console.log(`Server running on port ${port}`);
+});
